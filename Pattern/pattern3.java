807f24383694205f1fc7b10b17ba94c1ea53d741
Vikash Kumar <vikash3433@gmail.com>
2026-04-07 12:06:06 +0530

    Add pattern3.java to print number patterns

---
+++ Pattern/pattern3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class NewClass1 {
+    public static void main(String[] args) {
+       
+      int count = 1;
+      for (int i=1; i<=4; i++){
+        for (int j=1; j<=i; j++){
+          
+          System.out.print((count++)+" ");
+        } 
+        System.out.println("");
+      }
+    }
+}
